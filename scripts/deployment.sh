ac1a679181e3158a46f12c4b0f75ce99b5c64d31
pli01 <filib@laposte.net>
2016-05-19 18:40:53 +0200

    fix deployment

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -3,5 +3,14 @@
 FILE=${1:-deployment/rally-admin-cloud.rc}
 NAME=${2:-my_cloud}
 
-rally deployment create --file=$FILE --name=${NAME}
+test -f $FILE || exit 1
 
+# supprime config existante
+rally deployment show $NAME && rally deployment destroy $NAME
+rally deployment create --file=$FILE --name=$NAME
+
+# verif acces keystone
+rally deployment use $NAME && rally deployment check $NAME
+
+# verif acces services images
+rally show images
